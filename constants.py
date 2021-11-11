15b34da94b7c4870b98f6a53c58aa01def2e26b6
svalench <chitsalex@gmail.com>
2021-11-11 14:16:59 +0300

    LDAP service MVP

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-LDAP_SERVER_HOST = 'ldap://alutech.local'
-LDAP_SERVER_PORT = 3268
-LDAP_ROOT_USER = 'adsync@alutech.local'
-LDAP_ROOT_PASSWORD = 'JiraProjectsSync0609@$'
+LDAP_SERVER_HOST = os.getenv('LDAP_SERVER_HOST')
+LDAP_SERVER_PORT = os.getenv('LDAP_SERVER_PORT')
+LDAP_ROOT_USER = os.getenv('LDAP_ROOT_USER')
+LDAP_ROOT_PASSWORD = os.getenv('LDAP_ROOT_PASSWORD')
--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from typing import Optional
 
 from fastapi import FastAPI
-from ldap3 import Server, Connection, ALL
+from ldap3 import Server, Connection, ALL, NTLM
+from ldap3.core.exceptions import LDAPException
 
-from constants import LDAP_SERVER_HOST, LDAP_SERVER_PORT, LDAP_ROOT_USER, LDAP_ROOT_PASSWORD
+from constants import LDAP_SERVER_HOST, LDAP_SERVER_PORT
 
-server = Server(host=LDAP_SERVER_HOST, port=LDAP_SERVER_PORT, get_info=ALL)
-conn = Connection(server, user=LDAP_ROOT_USER, password=LDAP_ROOT_PASSWORD, auto_bind=True, auto_referrals=False)
+server = Server(host=LDAP_SERVER_HOST, port=int(LDAP_SERVER_PORT), get_info=ALL)
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"myName": "сервис Active Directory"}
 
 
-@app.get('/connection/check/')
-def check_connection():
-    conn.bind()
+@app.get('/check/user/{username}/{password}')
+def check_connection(username: str, password: str) -> dict:
+    return {"status": _ldap_login(username, password), "username": username}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def _ldap_login(username, password):
+    """проверка пользователя в LDAP"""
+    try:
+        with Connection(server, user=username, password=password) as conn:
+            print(conn.result["description"])
+            return True
+    except LDAPException:
+        print('Нет соединения с LDAP server')
+        return False
